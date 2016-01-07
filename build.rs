2f7cbbc15c293657ab79c3a9d494c19defda1c6b
Jamie Winsor <jamie@vialstudios.com>
2016-01-07 13:14:16 -0800

    rust bindgen, you've fucking failed me

--- build.rs
+++ build.rs
@@ -1,24 +1,8 @@
-extern crate bindgen;
 extern crate pkg_config;
-
-use std::env;
-use std::path::Path;
 
 fn main() {
     match pkg_config::find_library("libarchive") {
-        Ok(lib) => generate_rust_bindings(&lib),
+        Ok(_) => (),
         Err(msg) => panic!("Unable to locate libarchive, err={:?}", msg),
     }
-}
-
-fn generate_rust_bindings(lib: &pkg_config::Library) {
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string());
-    let out = Path::new(&manifest_dir);
-    let file = Path::new(&out).join("src").join("ffi.rs");
-    let h = lib.include_paths[0].join("comp.h").to_string_lossy().into_owned();
-    let mut bindings = bindgen::builder();
-    bindings.link("archive");
-    bindings.emit_builtins();
-    bindings.header(h);
-    bindings.generate().unwrap().write_to_file(&file).unwrap();
 }
